1b61159bedfdeee95902029b0d8231135d395cd6
Stas Pikuza <stassb130@mail.ru>
2019-09-02 18:52:10 +0300

    commit

---
+++ index.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <!--Import materialize.css-->
+  <link type="text/css" rel="stylesheet" href="libs/material/css/materialize.min.css" media="screen,projection" />
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link type="text/css" rel="stylesheet" href="css/style.css" />
+  <!--Let browser know website is optimized for mobile-->
+  <title>ИП Чубенок Н.В.</title>
+
+</head>
+
+<body>
+  <!-- Шапка ПБ -->
+<div class="bg">
+  <div class="container">
+  <nav id="main">
+    <div class="nav-wrapper">
+      <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons icon-men">menu</i></a>
+      <a href="#" class="brand-logo">
+        <img src="images/kolosok.png" alt="">
+        <h4>ИП Чубенок Н.В.</h4>
+      </a>
+      <ul class="right hide-on-med-and-down menu-items">
+        <li><a href="index.php">Главная</a></li>
+        <li><a href="catalog.php">Каталог</a></li>
+        <li><a href="info.php">Информация</a></li>
+      </ul>
+    </div>
+  </nav>
+  <ul class="sidenav" id="mobile-demo">
+    <li><a href="index.php">Главная</a></li>
+    <li><a href="catalog.php">Каталог</a></li>
+    <li><a href="info.php">Информация</a></li>
+  </ul>
+  <!-- приветственный блок -->
+  <div class="row hello-block">
+    <div class="col s12 m6 l6 hello-txt">
+      <div class="gray-bg">
+      <h3>ИП Чубенок Н.В.</h3>
+       <p>Оптимальное соотношение <br>
+          цены и качества.
+       </p>
+       </div>
+    </div>
+    <div class="col s12 m6 l6 connect-block">
+      <div class="gray-bg">
+      <h4>Наш номер телефона: <br>
+            +375 (44) 517-90-55 <br>
+          Также вы можете написать <br>
+          нам на e-mail нажав на <br>
+            кнопку ниже
+      </h4>
+      <a href="#" class="btn-connect">Связаться</a>
+      </div>
+      </div>
+    </div>
+  </div>
+</div>
+  <!-- Разделитель -->
+  <div class="separator">
+    <h4>Почему многие выбирают именно нас?</h4>
+  </div>
+  <!-- Почему мы (блок) -->
+
+  <div class="explanation-block">
+    <div class="block-f">
+    <div class="explanation">
+      <img src="images/icons/1.png" alt="">
+      <h5>Производительность и нажёдность</h5>
+    </div>
+    <div class="explanation">
+      <img src="images/icons/2.png" alt="">
+      <h5>Безотказная работа</h5>
+    </div>
+    <div class="explanation">
+      <img src="images/icons/3.png" alt="">
+      <h5>Доступность</h5>
+    </div>
+    </div>
+  </div>
+
+<!-- Разделитель -->
+  <div class="separator">
+    <h4>Наши услуги</h4>
+  </div>
+
+  <!-- Наши услуги -->
+
+  <div class="parallax-container">
+        <div class="parallax"><img src="images/uslugi_bg.jpeg"></div>
+        <div class="services">
+          <h5>«ИП Чубенок Н.В.» к вашим услугам. Мы понимаем, как важны для вашего бизнеса качество и надежность.
+      Мы гарантируем, что все наши товары имеют высокое качество и продаются по доступной цене.
+      Вы получаете то, что вам нужно, и тогда, когда вам это нужно. От этого зависит наша репутация, а репутация в нашей отрасли — это всё.
+      Свяжитесь с нами и узнайте, как «ИП Чубенок Н.В.» может помочь вам.</h5>
+        <a href="#" class="btn-connect main-color">Связаться</a>
+        </div>
+      </div>
+
+      <!-- Разделитель -->
+      <div class="separator">
+        <h4>Контакты</h4>
+      </div>
+
+      <!-- Контакты -->
+      <div class="explanation-block">
+        <div class="block-f">
+        <div class="explanation">
+          <img src="images/icons/4.png" alt="">
+          <h5>Гомельская обл., Хойникский р-н, Стреличевский с/с, д. Губоревичи</h5>
+        </div>
+        <div class="explanation">
+          <img src="images/icons/5.png" alt="">
+          <h5>ipchybenok@mail.ru</h5>
+        </div>
+        <div class="explanation">
+          <img src="images/icons/6.png" alt="">
+          <h5>+375 (44) 517-90-55</h5>
+          <h5>МТС +375 (29) 256-95-97</h5>
+        </div>
+        </div>
+      </div>
+
+
+
+
+
+  <div id="contacts"></div>
+  <footer class="page-footer">
+  <div class="container">
+    <div class="row">
+      <div class="col s12 footer-links"><a href="index.php">Главная</a></div>
+      <div class="col s12 footer-links"><a href="index.php">Каталог</a></div>
+      <div class="col s12 footer-links"><a href="index.php">Информация</a></div>
+    </div>
+  </div>
+  <div class="footer-copyright">
+    <div class="container">
+    © 2019 ИП Чубенок Н.В. Все права защищены.
+    <a class="grey-text text-lighten-4 right" href="http://brickweb.ru" target="_blank">brickweb.ru</a>
+    </div>
+  </div>
+</footer>
+
+
+
+
+  <script type="text/javascript" src="libs/jq.min.js"></script>
+  <script type="text/javascript" src="libs/material/js/materialize.min.js"></script>
+  <script type="text/javascript" src="js/init.js"></script>
+  <script type="text/javascript" src="js/script.js"></script>
+
+
+</body>
+
+</html>
---
+++ info.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <!--Import materialize.css-->
+  <link type="text/css" rel="stylesheet" href="libs/material/css/materialize.min.css" media="screen,projection" />
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link type="text/css" rel="stylesheet" href="css/style.css" />
+  <link type="text/css" rel="stylesheet" href="css/info.css" />
+
+  <!--Let browser know website is optimized for mobile-->
+  <title>ИП Чубенок Н.В.</title>
+
+</head>
+
+<body>
+  <!-- Шапка -->
+
+  <nav id="main">
+    <div class="nav-wrapper">
+        <div class="container">
+      <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons icon-men">menu</i></a>
+      <a href="#" class="brand-logo hide-on-med-and-down">
+        <h4>ИП Чубенок Н.В.</h4>
+      </a>
+      <ul class="right hide-on-med-and-down menu-items">
+        <li><a href="index.php">Главная</a></li>
+        <li><a href="catalog.php">Каталог</a></li>
+        <li><a href="info.php">Информация</a></li>
+      </ul>
+    </div>
+  </nav>
+  <ul class="sidenav" id="mobile-demo">
+    <li><a href="#!">Главная</a></li>
+    <li><a href="#!">Каталог</a></li>
+    <li><a href="#!">Информация</a></li>
+  </ul>
+  </div>
+
+
+
+      <!-- Контакты -->
+      <div class="explanation-block">
+        <div class="block-f">
+        <div class="explanation">
+          <img src="images/icons/4.png" alt="">
+          <h5>Гомельская обл., Хойникский р-н, Стреличевский с/с, д. Губоревичи</h5>
+        </div>
+        <div class="explanation">
+          <img src="images/icons/5.png" alt="">
+          <h5>ipchybenok@mail.ru</h5>
+        </div>
+        <div class="explanation">
+          <img src="images/icons/6.png" alt="">
+          <h5>+375 (44) 517-90-55</h5>
+          <h5>МТС +375 (29) 256-95-97</h5>
+        </div>
+        </div>
+      </div>
+
+
+
+
+  <div id="contacts"></div>
+  <footer class="page-footer">
+  <div class="container">
+    <div class="row">
+      <div class="col s12 footer-links"><a href="index.php">Главная</a></div>
+      <div class="col s12 footer-links"><a href="index.php">Каталог</a></div>
+      <div class="col s12 footer-links"><a href="index.php">Информация</a></div>
+    </div>
+  </div>
+  <div class="footer-copyright">
+    <div class="container">
+    © 2019 ИП Чубенок Н.В. Все права защищены.
+    <a class="grey-text text-lighten-4 right" href="http://brickweb.ru" target="_blank">brickweb.ru</a>
+    </div>
+  </div>
+</footer>
+
+
+
+
+  <script type="text/javascript" src="libs/jq.min.js"></script>
+  <script type="text/javascript" src="libs/material/js/materialize.min.js"></script>
+  <script type="text/javascript" src="js/init.js"></script>
+  <script type="text/javascript" src="js/script.js"></script>
+
+
+</body>
+
+</html>
